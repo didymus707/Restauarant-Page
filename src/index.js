c9ea56f383e6923ac935054f10647fdf310c073b
Adewale Orotayo <didymus7007@gmail.com>
2020-10-22 18:50:12 +0100

    Put images needed for the project in src/images Updated webpack to process images used Did an hard coding of how the page will look like Created the nav components that will be bundled by webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('Testing Webpack');
+import '../styles/style.css';
+import nav from './components/nav';
+
+const content = document.querySelector('#content');
+content.appendChild(nav());
